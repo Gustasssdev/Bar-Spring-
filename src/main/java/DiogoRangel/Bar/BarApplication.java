022dcff871688c6d76e4e69dd447f353b3efaad1
Gustasssdev <gustavosousacarvalho875@gmail.com>
2025-11-28 15:30:34 -0300

    Conexão com o banco de dados funcionando

--- src/main/java/DiogoRangel/Bar/BarApplication.java
+++ src/main/java/DiogoRangel/Bar/BarApplication.java
@@ -1,18 +1,18 @@
 package DiogoRangel.Bar;
 
-import org.springframework.boot.SpringApplication;
-import org. springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot. SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework. context.annotation.ComponentScan;
+import org.springframework.data. jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure. domain.EntityScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "DiogoRangel.Bar")  // ✅ Escaneia todos os pacotes
-@EnableJpaRepositories(basePackages = "DiogoRangel.Bar.repository")  // ✅ Encontra os Repositories
-@EntityScan(basePackages = "DiogoRangel.Bar.model")  // ✅ Encontra as Entidades
+@ComponentScan(basePackages = "DiogoRangel.Bar")
+@EnableJpaRepositories(basePackages = "DiogoRangel.Bar.repository")
+@EntityScan(basePackages = {"DiogoRangel.Bar.model", "DiogoRangel.Bar.classes"})
 public class BarApplication {
 
     public static void main(String[] args) {
-        SpringApplication. run(BarApplication.class, args);
+        SpringApplication.run(BarApplication. class, args);
     }
 }
--- src/main/java/DiogoRangel/Bar/repository/UsuarioRepository.java
+++ src/main/java/DiogoRangel/Bar/repository/UsuarioRepository.java
@@ -4,5 +4,5 @@ import DiogoRangel.Bar.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    Usuario findByToken(String token);
+    Usuario findByUsername(String username);
 }
